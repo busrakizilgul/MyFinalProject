44e8f4916ff1e9654bd000fe081ca73df66c09f8
Büşra Kızılgül <1030516356@erciyes.edu.tr>
2021-02-11 00:01:05 +0300

    10 Şubat Ders Sonu

---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    //Temel voidler için başlangıç
+    public interface IResult
+    {
+        //yapmaya çalıştığın ekleme işlemi başarılı ise true, değilse false döndürür.
+        bool Success { get; } //get=sadece okunabilir demektir.
+
+        string Message { get; }
+    }
+}
